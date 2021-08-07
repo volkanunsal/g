45ba459fc12d65d440fcca6773dea24e757235c0
Volkan Unsal <spocksplanet@gmail.com>
2021-08-07 18:20:38 -0400

    Mark progress

--- src/graphql/directives/modelDirective.ts
+++ src/graphql/directives/modelDirective.ts
@@ -1,24 +1,7 @@
-import { getDirective, MapperKind, mapSchema } from '@graphql-tools/utils'
 import { GraphQLSchema } from 'graphql'
 
-const directiveName = 'model'
-
 export function modelDirectiveTransformer(schema: GraphQLSchema) {
-  let addBaseTypes = false
-  let baseTypeRoot: string | undefined
-  const newSchema = mapSchema(schema, {
-    [MapperKind.OBJECT_TYPE]: (fieldConfig) => {
-      const directive = getDirective(schema, fieldConfig, directiveName)
-      if (directive) {
-        addBaseTypes = true
-        const config = fieldConfig.toConfig()
-        baseTypeRoot = config.name
-      }
-      return fieldConfig
-    },
-  })
-
-  return newSchema
+  return schema
 }
 
 export const modelDirectiveTypeDefs = `
